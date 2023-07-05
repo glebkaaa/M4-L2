454a949bbb4db951fea136d651f6a8253f520adc
glebkaaa <glebguslakov6@gmail.com>
2023-07-05 09:22:29 +0600

    update main.py

--- main.py
+++ main.py
@@ -1,30 +1,18 @@
-#Импорт
 from flask import Flask, render_template,request, redirect
-#Подключение библиотеки баз данных
 from flask_sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
-#Подключение SQLite
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///diary.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-#Создание db
+
 db = SQLAlchemy(app)
-#Создание таблицы
 
 class Card(db.Model):
-    #Создание полей
-    #id
     id = db.Column(db.Integer, primary_key=True)
-    #Заголовок
     title = db.Column(db.String(100), nullable=False)
-    #Описание
     subtitle = db.Column(db.String(300), nullable=False)
-    #Текст
     text = db.Column(db.Text, nullable=False)
-
-    #Вывод объекта и id
-    
     
 
 #Задание №1. Создать таблицу User
@@ -35,10 +23,6 @@ class User(db.Model):
 
     def __repr__(self):
         return f'<Card {self.id}>'
-
-
-
-
 
 
 #Запуск страницы с контентом
@@ -72,9 +56,7 @@ def reg():
         user = User(login=login, password=password)
         db.session.add(user)
         db.session.commit()
-        
         return redirect('/')
-    
     else:
         return render_template('registration.html')
 
@@ -82,32 +64,27 @@ def reg():
 #Запуск страницы с контентом
 @app.route('/index')
 def index():
-    #Отображение объектов из БД
     cards = Card.query.order_by(Card.id).all()
     return render_template('index.html', cards=cards)
 
-#Запуск страницы c картой
+
 @app.route('/card/<int:id>')
 def card(id):
     card = Card.query.get(id)
-
     return render_template('card.html', card=card)
 
-#Запуск страницы c созданием карты
+
 @app.route('/create')
 def create():
     return render_template('create_card.html')
 
-#Форма карты
+
 @app.route('/form_create', methods=['GET','POST'])
 def form_create():
     if request.method == 'POST':
         title =  request.form['title']
         subtitle =  request.form['subtitle']
         text =  request.form['text']
-
-        #Создание объкта для передачи в дб
-
         card = Card(title=title, subtitle=subtitle, text=text)
 
         db.session.add(card)
@@ -117,8 +94,5 @@ def form_create():
         return render_template('create_card.html')
 
 
-
-
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(debug=True)
